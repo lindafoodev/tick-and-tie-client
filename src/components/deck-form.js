14b487728bc4d5e95a981131dd44a61d9cd6ab04
Linda Foo <linda.foo.dev@gmail.com>
2018-04-25 22:59:18 -0700

    test render

--- src/components/deck-form.js
+++ src/components/deck-form.js
@@ -5,6 +5,25 @@ import {required, nonEmpty} from '../validators';
 import {sendAnswer} from '../actions/deck';
 
 export class DeckForm extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+          showForm: true
+        };
+      }
+
+    handleClick(){
+    this.setState({
+        showForm: true
+    });
+    this.props.reset();
+    }
+
+    handleCard(){
+    this.setState({
+        showForm: false
+    });
+    }
 
     onSubmit(values) {
         console.log('what is values', values);
@@ -12,6 +31,7 @@ export class DeckForm extends React.Component {
     }
 
     render() {
+        let card;
         let error;
         if (this.props.error) {
             error = (
@@ -21,24 +41,35 @@ export class DeckForm extends React.Component {
             );
         }
         let feedback;
-        if(this.props.feedback) {
-          if(this.props.inputAnswer){
-            feedback = (
-                <p className="tag-line">Correct!</p>
-            );
-          }
-          if(!this.props.inputAnswer) {
-            feedback =  (
-                <p className="tag-line">Incorrect!<br/>Answer<br/> <span className="emphasis-answer">{this.props.currentAnswer}</span></p>
-            );
-          }
-      }
-
-        return (
-          <div className='home deck-form-template'>
+        if(!this.props.showForm){
+            if(this.props.feedback) {
+            if(this.props.inputAnswer){
+                feedback = (
+                    <div>
+                        <p className="tag-line">Correct!</p>
+                            <button className="next-button" onClick={this.handleClick.bind(this)}>
+                                Next Question
+                            </button>
+                    </div>
+                );
+            }
+            if(!this.props.inputAnswer) {
+                feedback =  (
+                    <div>
+                        <p className="tag-line">Incorrect!<br/>Answer<br/> <span className="emphasis-answer">{this.props.currentAnswer}</span></p>
+                            <button className="next-button" onClick={this.handleClick.bind(this)}>
+                                Next Question
+                            </button>
+                    </div>
+                );
+                }
+            }
+        }
+      if(this.state.showForm){
+        card = (
+            <div className='card'>
             <div className='sideA'>
                <p className='sideA'>{this.props.sideA}</p>
-               <div>{feedback}</div>
             </div>
             <form
                 className="deck-form"
@@ -55,11 +86,19 @@ export class DeckForm extends React.Component {
                     placeholder="enter answer here"
                     validate={[required, nonEmpty]}
                 />
-                <button className="answer-button" disabled={this.props.pristine || this.props.submitting}>
+                <button className="answer-button" type="button" onClick={this.handleCard.bind(this)} disabled={this.props.pristine || this.props.submitting}>
                    Submit 
                 </button>
             </form>
-          </div>
+            </div>
+        );
+    }
+
+        return (
+            <div className='home deck-form-template'>
+                {card}
+                {feedback}
+            </div>
         );
     }
 }
