a715df77be6dfe1bc7e700d90e4bb7c5a2568246
Linda Foo <33673643+lindafoodev@users.noreply.github.com>
2018-07-13 13:05:27 -0700

    added demo login option on registration page

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
 
 import RegistrationForm from './registration-form';
+import {login} from '../actions/auth';
 
 export function RegistrationPage(props) {
     // If we are logged in (which happens automatically when registration
@@ -10,12 +11,20 @@ export function RegistrationPage(props) {
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
     }
+    
+    const demoLogIn = (obj) => {
+        return props.dispatch(login(obj.username, obj.password));
+    }
+       
     return (
         <div className="home">
             <h2>Register</h2>
             <RegistrationForm />
             <div className='redirect'>
                 Returning user? <Link to="/login">Log in</Link>
+            </div>
+            <div className='redirect'>
+                Or try out the <Link to="/dashboard" onClick={() => demoLogIn({username:'guest', password:'guest12345'})}>Demo</Link> account.
             </div>
         </div>
     );
